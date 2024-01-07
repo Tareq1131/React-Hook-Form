e3a744f6945cc42693b7fb61476e868fcabf1625
Tareq1131 <tareqislam076@gmail.com>
2024-01-07 15:06:38 +0600

    Display Error Messages

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -10,8 +10,9 @@ let renderCount = 0;
 const YouTubeForm = () => {
 
   const form = useForm<FormValues>()
-  const { register, control, handleSubmit } = form
+  const { register, control, handleSubmit, formState } = form
   // const { name, ref, onChange, onBlur } = register('username')
+  const { errors } = formState;
 
   const onSubmit = (data: FormValues) => {
     console.log(data);
@@ -23,6 +24,7 @@ const YouTubeForm = () => {
       <h1>YouTube Form({renderCount / 2})</h1>
 
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className='form-control'>
         <label htmlFor="username">Username</label>
         <input type="text"
           id="username"
@@ -33,23 +35,32 @@ const YouTubeForm = () => {
           {...register('username', {
             required: {
               value: true,
-              message:"Username is required"
+              message: "Username is required"
             }
           })}
         />
+        <p className="error">{errors.username?.message}</p>
 
+        </div>
+       
+       <div className='form-control'>
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register('email',{
+        <input type="email" id="email" {...register('email', {
           pattern: {
-              value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              message: "Invalid email format",
+            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+            message: "Invalid email format",
           }
         })} />
+        <p className="error">{errors.email?.message}</p>
+        </div>
 
+        <div className='form-control'>
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register('channel', {
-            required: "channel is required"
-          })} />
+          required: "channel is required"
+        })} />
+          <p className="error">{errors.channel?.message}</p>
+          </div>
         <button>Submit</button>
       </form>
       <DevTool control={control} />
