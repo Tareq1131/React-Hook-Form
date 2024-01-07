61b68bcfb1dce09f4850c860adddd4da2440a872
Tareq1131 <tareqislam076@gmail.com>
2024-01-07 14:08:06 +0600

    useForm hook and Managing Form State

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import YouTubeForm from './components/YouTubeForm'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <YouTubeForm/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,35 @@
+import { useForm } from 'react-hook-form'
+
+const YouTubeForm = () => {
+
+  const form = useForm()
+  const { register } = form
+  // const { name, ref, onChange, onBlur } = register('username')
+  return (
+    <div>
+      <h1>YouTube Form</h1>
+
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text"
+          id="username"
+          // name={name}
+          // ref={ref}
+          // onChange={onChange}
+          // onBlur={onBlur}
+          {...register('username')}
+        />
+
+        <label htmlFor="email">E-mail</label>
+        <input type="email" id="email" {...register('email')} />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" {...register('channel')} />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default YouTubeForm;
